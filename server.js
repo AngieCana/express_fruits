ab7015e9d308cfc3a3438eab0812e9581fe7c4f3
AngieCana <grilozangie@gmail.com>
2023-08-16 13:51:22 -0400

    updated code fixed server not working

--- server.js
+++ server.js
@@ -1,38 +1,31 @@
 const express = require("express");
-//import express = from ('express`)
-const fruits = require("./components/fruits.js");
-const veggies = require("./components/veggies.js");
-
+const jsxViewEngine = require("jsx-view-engine");
+const fruits = require("./components/fruits")
+const veggies = require("./components/veggies")
 const app = express();
-
 app.set("view engine", "jsx");
-app.engine("jsx", require("jsx-view-engine").createEngine());
+app.engine("jsx", jsxViewEngine());
 
-//Define Routes
+// Define middleware
+app.use((req, res, next) => {
+  console.log("I run for all routes");
+  next();
+});
 
+app.use(express.urlencoded({ extended: false }));
+// Define routes
 app.get("/", (req, res) => {
-  res.send("Home page of fruits and veggies.");
+  res.send("Home page of Fruits and Veggies");
 });
 
 app.get("/fruits/", (req, res) => {
-  res.send("Index", { fruits: fruits });
+  // res.send(fruits)
+  res.render("Index", { fruits: fruits });
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.get("/fruits/new", (req, res) => {
+  res.render("New");
 });
-
-//view body of post request
-//near the top, around other app.use() calls
-app.use(express.urlencoded({ extended: false }));
-
-//create post request to fruits
-app.post("/fruits", (req, res) => {
-  console.log(req.body);
-  res.send("data received");
-});
-
-//push data into fruits array
 app.post("/fruits", (req, res) => {
   if (req.body.readyToEat === "on") {
     //if checked, req.body.readyToEat is set to 'on'
@@ -43,41 +36,23 @@ app.post("/fruits", (req, res) => {
   }
   fruits.push(req.body);
   console.log(fruits);
+  res.redirect("/fruits"); // sends user back to fruits page, /fruits
   res.send("data received");
 });
-
-//send user back to fruits index upon completion
-app.post("/fruits", (req, res) => {
-  if (req.body.readyToEat === "on") {
-    //if checked, req.body.readyToEat is set to 'on'
-    req.body.readyToEat = true;
-  } else {
-    //if not checked, req.body.readyToEat is undefined
-    req.body.readyToEat = false;
-  }
-  fruits.push(req.body);
-  res.redirect("/fruits"); //send the user back to /fruits
-});
-
-//have a function that executes all routes
-app.use((req, res, next) => {
-  console.log("I run for all routes");
-  next();
-});
-
-//route to create new food
-app.get("/fruits/new", (req, res) => {
-  res.render("New");
-});
-
-//create a show route (show routes use a get request)
+// Create a SHOW route (show routes use a get request)
 app.get("/fruits/:indexOfFruitsArray", (req, res) => {
   // res.send(fruits[req.params.indexOfFruitsArray]);
-  res.render("Show");
-  //include a second param that must be an object (req.params.index)
-  fruit: fruits[req.params.indexOfFruitsArray];
+  res.render("Show", {
+    // include a second param that must be an object (req.params.indexOfFruitsArray)
+    fruit: fruits[req.params.indexOfFruitsArray],
+  });
 });
-
-app.get("/veggies/:indexOfVeggiesArray", (req, res) => {
-  res.send(veggies[req.params.indexOfVeggiesArray]);
+app.get("/veggies/", (req, res) => {
+  res.send(veggies);
+});
+app.get("/veggies/:indexOfVegetablesArray", (req, res) => {
+  res.send(veggies[req.params.indexOfVegetablesArray]);
+});
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,31 +1,29 @@
 const React = require("react");
 
-class Index extends React.Component {
-  render() {
-    const { fruits } = this.props;
-    return (
-      <div>
-        <h1>Fruits Index Page</h1>
-        <nav>
-          <a href="/fruits/new">Create a New Fruit</a>
-        </nav>
-        <ul>
-          {fruits.map((fruit, i) => {
-            return (
-              <li>
-                The {""}
-                <a href={`/fruits/${i}`}></a>
-                {""}
-                is {fruit.color} <br></br>
-                {fruit.readyToEat
-                  ? `It is ready to eat`
-                  : `It is not ready to eat.`}
-                <br />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+const Index = (props) => {
+  const { fruits } = props;
+  return (
+    <div>
+      <h1>Fruits Index Page</h1>
+      <ul>
+        {fruits.map((fruit, i) => {
+          return (
+            <li>
+              The {""}
+              <a href={`/fruits/${i}`}>{fruit.name}</a>
+              {""}
+              is {fruit.color} <br></br>
+              {fruit.readyToEat ? `It is ready to eat` : `Nope it is not ready`}
+              <br />
+            </li>
+          );
+        })};
+        
+      </ul>
+      <nav>
+        <a href="/fruits/new">Create a New Fruit</a>
+      </nav>
+    </div>
+  );
+};
+module.exports = Index;
